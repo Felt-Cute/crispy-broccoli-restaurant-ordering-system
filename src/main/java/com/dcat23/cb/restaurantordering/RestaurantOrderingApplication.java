cab27bde9eb4fad40c19bac129c24eeab7297adc
Devin Catuns <devincatuns1@gmail.com>
2024-07-18 10:35:03 -0400

    first commit

---
+++ src/main/java/com/dcat23/cb/restaurantordering/RestaurantOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.dcat23.cb.restaurantordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantOrderingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dcat23/cb/restaurantordering/RestaurantOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcat23.cb.restaurantordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
